8fdaaedbc961784728cb6e48f609f2d2398c4c7e
Temp User <temp@example.com>
2025-04-09 13:57:35 +0500

    implemented secure api key access in the pydantic-ai.py  file

--- pydantic_ai_implementaiton.py
+++ pydantic_ai_implementaiton.py
@@ -3,11 +3,18 @@ from pydantic_ai.models.groq import GroqModel
 from pydantic_ai.providers.groq import GroqProvider
 from pydantic_ai.mcp import  MCPServerStdio
 
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the API key from the environment
+api_key = os.getenv('GROQ_API_KEY')
 
 model = GroqModel(
-    'qwen-2.5-32b', provider=GroqProvider(api_key='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')
+    'qwen-2.5-32b', provider=GroqProvider(api_key=api_key)
 )
-
 fetchserver = MCPServerStdio('python', ["-m", "mcp_server_fetch"])
 
 
